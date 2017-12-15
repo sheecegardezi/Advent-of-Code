b73bb1c08d73333ae4dc03cc4a5eb78613b02123
Syed Sheece Raza Gardezi <sheecegardezi@users.noreply.github.com>
2017-12-15 15:24:47 +1100

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,4 +1,24 @@
-#convert the input text into python datastructure#return [[list,of,numbers,......],[...],...]def extractDataStructure():        dataStructure=[]    file = open("input/day2.txt", "r")        for line in file:        row=[]        for letter in line.split('\t'):            row.append(int(letter))        dataStructure.append(row)            return dataStructure
-#main programdataStructure=extractDataStructure()
-#sum the difference between max and min value of each rowanswer= sum ([ max(row)-min(row) for row in dataStructure ])
-#outputprint(answer)
+#convert the input text into python datastructure
+#return [[list,of,numbers,......],[...],...]
+def extractDataStructure():
+    
+    dataStructure=[]
+    file = open("input/day2.txt", "r")
+    
+    for line in file:
+        row=[]
+        for letter in line.split('\t'):
+            row.append(int(letter))
+        dataStructure.append(row)
+        
+    return dataStructure
+
+
+#main program
+dataStructure=extractDataStructure()
+
+#sum the difference between max and min value of each row
+answer= sum ([ max(row)-min(row) for row in dataStructure ])
+
+#output
+print(answer)
