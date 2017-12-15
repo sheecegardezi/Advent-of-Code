41eeb8af73d87611f96c0c9e2860a216589d069d
Syed Sheece Raza Gardezi <sheecegardezi@users.noreply.github.com>
2017-12-15 15:25:07 +1100

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,4 +1,28 @@
-#convert the input text into python datastructure#return [list,of,numbers,......]def extractDataStructure():        dataStructure=[]    file = open("input/day1.txt", "r")        for line in file:        for letter in line:            dataStructure.append(int(letter))            return dataStructure
-#main programdataStructure=extractDataStructure()
+#convert the input text into python datastructure
+#return [list,of,numbers,......]
+def extractDataStructure():
+    
+    dataStructure=[]
+    file = open("input/day1.txt", "r")
+    
+    for line in file:
+        for letter in line:
+            dataStructure.append(int(letter))
+        
+    return dataStructure
+
+#main program
+dataStructure=extractDataStructure()
+
 answer=0
-#add current value to answer if the next value in equal to the current valuefor i in range(0,len(dataStructure)-1):    if dataStructure[i] == dataStructure[i+1]:        answer=answer+dataStructure[i]#handle last elementif dataStructure[0] == dataStructure[-1]:    answer=answer+dataStructure[0]    #outputprint(answer)
+
+#add current value to answer if the next value in equal to the current value
+for i in range(0,len(dataStructure)-1):
+    if dataStructure[i] == dataStructure[i+1]:
+        answer=answer+dataStructure[i]
+#handle last element
+if dataStructure[0] == dataStructure[-1]:
+    answer=answer+dataStructure[0]
+    
+#output
+print(answer)
