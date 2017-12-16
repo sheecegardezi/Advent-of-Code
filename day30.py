e5714ef8e0193b626e81e95a378fe1d31f8c5f56
Syed Gardezi <u5752631@uds.anu.edu.au>
2017-12-16 22:17:09 +1100

    add header

--- day30.py
+++ day30.py
@@ -1,18 +1,33 @@
+#!/usr/bin/env python
+# # -*- coding: utf-8 -*-
 """
-link to problem: http://adventofcode.com/2017/day/3
-author: sheece gardezi
+title           :day30.py
+description     :Link to problem: http://adventofcode.com/2017/day/3
+author          :Sheece Gardezi
+date            :2017/16/12
+version         :1.0
+usage           :python day30.py
+python_version  :3.6.1
 """
+
+# Import the modules needed to run the script.
 import math
+
 def getDimentionOfGrid(number):
+    """Return the length of side of rectangle."""
+    """
+    find the length of side of recangle in which the number exists 
+    using the fact that max number in each rectange will be square of odd number 
+    """
+    
     i = 1
     while number > math.pow(i, 2):
         i=i+2
     return i
 
+#main program
 input=361527
 
-#find the length of side of recangle in which the number exists 
-#using the fact that max number in each rectange will be square of odd number 
 dimentions=getDimentionOfGrid(input)
 
 #calaculte the corner value of the rectangle
