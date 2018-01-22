3492adedfee0b6f3c42f23b03e8ed70b34ac1ead
Syed Gardezi <Syed.Gardezi@evansdixon.com.au>
2018-01-22 15:42:01 +1100

    rearrange files

---
+++ C#/50.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\users\syed.gardezi\documents\visual studio 2015\Projects\AdventOfCode\AdventOfCode\input\50.txt";
+
+            List<Int32> listOfJumps = new List<int>();
+            // Open the text file using a stream reader.
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadLine().Trim();
+                    listOfJumps.Add(Int32.Parse(line));
+                        
+                }
+            }
+            
+            int jump = 0;
+            int count = 0;
+
+            
+            
+            while (jump<listOfJumps.Count) {
+
+
+                var oldjump = jump;
+                jump = jump + listOfJumps[jump];
+                listOfJumps[oldjump] = listOfJumps[oldjump] + 1;
+                count++;
+            }
+
+    
+            Console.WriteLine("Count:"+count);
+
+
+
+
+        }
+    }
+}
